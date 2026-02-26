a19e9ac1876d870aa3fd2d0e7f0c14cc2c5cb7f0
Sujeet Banerjee <sujeet.banerjee@gmail.com>
2026-02-26 14:05:39 +0530

    Day 1: Fixed test context and verified health heartbeat; updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # sentinel
+
+
+
+## sentinel-api
+
+This is springboot based API layer.
+
+### Dev CLI
+
+./mvnw spring-boot:run
+./mvnw clean test
+
--- sentinel-api/src/test/java/com/sentinel/api/SentinelApiApplicationTests.java
+++ sentinel-api/src/test/java/com/sentinel/api/SentinelApiApplicationTests.java
@@ -1,13 +1,45 @@
 package com.sentinel.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.sentinel.api.config.SentinelApiApplication;
+
+/*
+ * We explicitly point to the class here, to avoid error during test-run:
+ * [ERROR] Errors:
+   [ERROR]  SentinelApiApplicationTests » IllegalState Unable to find a 
+   			@SpringBootConfiguration by searching packages upwards from 
+   			the test. You can use @ContextConfiguration, 
+   			@SpringBootTest(classes=...) or other Spring Test supported 
+   			mechanisms to explicitly declare the configuration classes 
+   			to load. Classes annotated with @TestConfiguration are not considered.
+ */
+
+@SpringBootTest(
+		classes = SentinelApiApplication.class,
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class SentinelApiApplicationTests {
+	
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
 	void contextLoads() {
+	    // Basic check to ensure the Spring context starts up
 	}
+	
+	@Test
+    void healthCheckShouldReturnOnline() {
+        webTestClient.get().uri("/health")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .isEqualTo("SENTINEL ENGINE: ONLINE");
+    }
 
 }
